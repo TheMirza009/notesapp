a9a9f40eaa265e487c07209f07299aa045e85822
themirza009 <mirzaabdulmoed@gmail.com>
2025-09-14 04:37:24 +0500

    Isolated Chat Screen Test UI built

--- lib/root/screens/Load_test/isar_test.dart/screens/test_chat_screen.dart
+++ lib/root/screens/Load_test/isar_test.dart/screens/test_chat_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/core/Theme/theme_constants.dart';
 import 'package:notesapp/root/data/models/chat_model.dart';
 import 'package:notesapp/root/data/models/message_model.dart';
+import 'package:notesapp/root/screens/Chat_Screen/components/bottom_message_bar.dart';
 import 'package:notesapp/root/screens/Chat_Screen/components/message_bubbles.dart/message_bubble_wrapper.dart';
+import 'package:notesapp/root/widgets/nothing_to_see.dart';
 
 class TestChatScreen extends StatefulWidget {
   final Chat chat;
@@ -20,10 +22,10 @@ class _TestChatScreenState extends State<TestChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ThemeConstants.messageBarDark,
+      backgroundColor: ThemeConstants.textLight,
       appBar: AppBar(
         title: Text("Chat: ${widget.chat.title}"),
-        backgroundColor: Colors.transparent,
+        backgroundColor: ThemeConstants.messageBarDark,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           tooltip: '', // <-- disable tooltip
@@ -34,13 +36,21 @@ class _TestChatScreenState extends State<TestChatScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
+            child: messages.isEmpty 
+            ? NothingToSee() 
+            : ListView.builder(
               itemCount: messages.length,
               itemBuilder: (context, index) {
                 final Message message = messages[index];
               return MessageBubble(message: message);
             }),
           ),
+          BottomMessageBar(
+            onEmojiTap: () {},
+            onAttachmentTap: () {},
+            onMicTap: () {},
+            onSend: (text) {},
+          )
         ],
       ),
     );
