70603c0b942601d05c89935046807763c0935e20
themirza009 <mirzaabdulmoed@gmail.com>
2026-06-02 15:54:11 +0500

    minSdk changed to attempt Live Production deployment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         applicationId = "com.azdhaar.notesapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24 // flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
